9c1666d0312c39426fc4e41370611c0f0c620cb5
Sean Tayler <seantayler@gAM7acd39.local>
2019-09-10 10:48:22 -0700

    init

---
+++ tmpm-Spring/src/main/java/com/seantayler/tmpm/TmpmApplication.java
@@ -0,0 +1,13 @@
+package com.seantayler.tmpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmpmApplication.class, args);
+	}
+
+}
