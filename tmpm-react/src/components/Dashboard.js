d1df6ea8917eb28dc4df590700d31145a77aa50d
Sean Tayler <seantayler@gAM7acd39.local>
2019-09-19 12:53:41 -0700

    updated App.js and added routes

--- tmpm-react/src/components/Dashboard.js
+++ tmpm-react/src/components/Dashboard.js
@@ -3,15 +3,26 @@ import ProjectItem from './project/ProjectItem';
 import Header from './layout/Header';
 
 class Dashboard extends React.Component {
-    render(){
-        return (
-          <div>
-              <Header />
-              Welcome
-              <ProjectItem />
+    render() {
+      return (
+        <div className="projects">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 text-center">Projects</h1>
+                <br />
+                <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                  Create a Project
+                </a>
+                <br />
+                <hr />
+                <ProjectItem />
+              </div>
+            </div>
           </div>
-        );
+        </div>
+      );
     }
-}
+  }
 
 export default Dashboard;
