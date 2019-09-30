6449e3bf64404dc15001aca49f9818a272c33b01
Sean Tayler <seantayler@gAM7acd39.local>
2019-09-29 20:42:17 -0700

    UI completed

--- tmpm-react/src/App.js
+++ tmpm-react/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
-import Header from "./components/layout/Header";
+import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import AddProject from "./components/project/AddProject";
+import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
-import UpdateProject from "./components/project/UpdateProject";
+import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
+import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
 
 class App extends Component {
   render() {
@@ -19,6 +22,17 @@ class App extends Component {
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
+            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+            <Route
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            />
+            <Route
+              exact
+              path="/updateProjectTask/:backlog_id/:pt_id"
+              component={UpdateProjectTask}
+            />
           </div>
         </Router>
       </Provider>
